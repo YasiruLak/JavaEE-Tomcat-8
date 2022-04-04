fcdeebf4ed3811208113cc9787a5ed27c49a7b18
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-04 16:47:05 +0530

    Initial commit

---
+++ JavaEE/01_HelloServlet/src/HelloServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : JavaEE
+ * @date : 4/4/2022
+ * @month : 04
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request ekak labuna..");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response Sended..");
+    }
+}
