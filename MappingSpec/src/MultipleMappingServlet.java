5702a2035120390409125fafe03bcf4bd5583c64
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-18 10:57:27 +0530

    Mapping Servlet

---
+++ MappingSpec/src/MultipleMappingServlet.java
@@ -0,0 +1,10 @@
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : JavaEE-Tomcat-8
+ * @date : 4/18/2022
+ * @month : 04
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+public class MultipleMappingServlet {
+}
