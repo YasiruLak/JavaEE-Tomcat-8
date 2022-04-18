4f9c3cc69f0d02da0162cb50b362238ae6d07a2f
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-18 11:01:31 +0530

    Extension Mapping

--- MappingSpec/src/ExtensionMappingServlet.java
+++ MappingSpec/src/ExtensionMappingServlet.java
@@ -1,3 +1,10 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author : Yasiru Dahanayaka
  * @name : JavaEE-Tomcat-8
@@ -6,5 +13,12 @@
  * @year : 2022
  * @since : 0.1.0
  **/
-public class ExtensionMappingServlet {
+
+//Extension Mapping // jpg, gif, mp4
+@WebServlet(urlPatterns = "*.ijse")
+public class ExtensionMappingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Extension Mapping Spec Invoked");
+    }
 }
