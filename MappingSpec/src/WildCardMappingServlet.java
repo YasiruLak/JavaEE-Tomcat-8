b744ac678343740a97abcefe877f4331d169b2f8
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-18 11:03:29 +0530

    WildCard Mapping

--- MappingSpec/src/WildCardMappingServlet.java
+++ MappingSpec/src/WildCardMappingServlet.java
@@ -1,3 +1,10 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author : Yasiru Dahanayaka
  * @name : JavaEE-Tomcat-8
@@ -6,5 +13,23 @@
  * @year : 2022
  * @since : 0.1.0
  **/
-public class WildCardMappingServlet {
+
+//Wild Card Mapping // /items/I001/I002
+@WebServlet(urlPatterns = "/items/*")
+public class WildCardMappingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String servletPath = req.getServletPath();
+        String method = req.getMethod();
+        String pathInfo = req.getPathInfo();
+        String contextPath = req.getContextPath();
+
+        System.out.println("Wild Mapping Spec Invoked");
+        System.out.println("==========================");
+        System.out.println("Servlet path "+servletPath);
+        System.out.println("Method "+method);
+        System.out.println("Path Info  "+pathInfo);
+        System.out.println("Context Path  "+contextPath);
+        System.out.println("==========================");
+    }
 }
