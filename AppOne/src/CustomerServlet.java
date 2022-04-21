fb1419a187c44390226bc7b8cca0e76fa971d7c4
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-21 19:35:12 +0530

    Get Data Jason

--- AppOne/src/CustomerServlet.java
+++ AppOne/src/CustomerServlet.java
@@ -27,15 +27,22 @@ public class CustomerServlet extends HttpServlet {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company","root", "19980611");
             ResultSet resultSet = connection.prepareStatement("select * from Customer").executeQuery();
+            String allRecords = "";
             while (resultSet.next()){
                 String id = resultSet.getString(1);
                 String name = resultSet.getString(2);
                 String address = resultSet.getString(3);
                 Double salary = resultSet.getDouble(4);
-                System.out.println(id + " " + name + " " + address + " " + salary);
+//                System.out.println(id + " " + name + " " + address + " " + salary);
+
+                String customer = "{\"id\":\"" + id + "\",\"name\":\"" + name + "\",\"address\":\"" + address + "\",\"salary\":" + salary + "},";
+                allRecords = allRecords + customer;
             }
 
+            String finalJson = "[" + allRecords.substring(0,allRecords.length()-1) + "]";
+
             PrintWriter writer = resp.getWriter();
+            writer.write(finalJson);
 
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
