ebfb9a8e3bd4b8175842cfe0982ce746e4895ef9
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-23 19:10:20 +0530

    get all item in db

--- AppOne/src/ItemServlet.java
+++ AppOne/src/ItemServlet.java
@@ -13,30 +13,30 @@ import java.sql.SQLException;
 /**
  * @author : Yasiru Dahanayaka
  * @name : JavaEE-Tomcat-8
- * @date : 4/18/2022
+ * @date : 4/23/2022
  * @month : 04
  * @year : 2022
  * @since : 0.1.0
  **/
-@WebServlet(urlPatterns = "/customer")
-public class CustomerServlet extends HttpServlet {
+
+@WebServlet(urlPatterns = "/item")
+public class ItemServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company","root", "19980611");
-            ResultSet resultSet = connection.prepareStatement("select * from Customer").executeQuery();
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "19980611");
+            ResultSet resultSet = connection.prepareStatement("select * from Item").executeQuery();
+
             String allRecords = "";
             while (resultSet.next()){
-                String id = resultSet.getString(1);
+                String code = resultSet.getString(1);
                 String name = resultSet.getString(2);
-                String address = resultSet.getString(3);
-                double salary = resultSet.getDouble(4);
-//                System.out.println(id + " " + name + " " + address + " " + salary);
+                int qty = resultSet.getInt(3);
+                double price = resultSet.getDouble(4);
 
-                String customer = "{\"id\":\"" + id + "\",\"name\":\"" + name + "\",\"address\":\"" + address + "\",\"salary\":" + salary + "},";
-                allRecords = allRecords + customer;
+                String item = "{\"code\":\"" + code + "\",\"name\":\"" + name + "\",\"qty\":\"" + qty + "\",\"price\":" + price + "},";
+                allRecords = allRecords + item;
             }
 
             String finalJson = "[" + allRecords.substring(0,allRecords.length()-1) + "]";
@@ -44,10 +44,9 @@ public class CustomerServlet extends HttpServlet {
             PrintWriter writer = resp.getWriter();
             writer.write(finalJson);
 
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
-
 
     }
 }
