99a7b64d1861735b8cac39729fb44106dc8adec1
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-07 23:05:35 +0530

    Database Pool Updated

--- DBCP/src/CustomerServlet.java
+++ DBCP/src/CustomerServlet.java
@@ -28,9 +28,9 @@ public class CustomerServlet extends HttpServlet {
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/company");
         basicDataSource.setUsername("root");
         basicDataSource.setPassword("19980611");
-        basicDataSource.setUrl("jdbc:mysql://localhost:3306/company");
         basicDataSource.setMaxTotal(5);
         basicDataSource.setInitialSize(5);
 
@@ -43,6 +43,7 @@ public class CustomerServlet extends HttpServlet {
                 String id = resultSet.getString(1);
                 System.out.println(id);
             }
+            connection.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
