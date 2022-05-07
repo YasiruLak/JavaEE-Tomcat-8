6df94b1e5449dbfed8769239d48f94c7f2a8daf3
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-07 22:30:13 +0530

    Database Pool Add

--- DBCP/src/CustomerServlet.java
+++ DBCP/src/CustomerServlet.java
@@ -1,9 +1,15 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author : Yasiru Dahanayaka
@@ -20,6 +26,26 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("19980611");
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/company");
+        basicDataSource.setMaxTotal(5);
+        basicDataSource.setInitialSize(5);
 
+        try {
+            Connection connection = basicDataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("Select * from Customer");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String id = resultSet.getString(1);
+                System.out.println(id);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
