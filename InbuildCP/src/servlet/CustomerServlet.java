9d7c30009e0a1fc33408b63132d27203f41be391
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-08 18:46:11 +0530

    InbuildCp doGet Update

--- InbuildCP/src/servlet/CustomerServlet.java
+++ InbuildCP/src/servlet/CustomerServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import javax.annotation.Resource;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -31,13 +35,40 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from Customer");
-            ResultSet resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()) {
-                String id = resultSet.getString(1);
-                System.out.println(id);
+            String option = req.getParameter("option");
+            resp.setContentType("application/json");
+            Connection connection = dataSource.getConnection();
+            PrintWriter writer = resp.getWriter();
+            switch (option) {
+                case "SEARCH":
+
+                    break;
+
+                case "GETALL":
+                    PreparedStatement preparedStatement = connection.prepareStatement("select * from Customer");
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
+                    while (resultSet.next()) {
+                        String id = resultSet.getString(1);
+                        String name = resultSet.getString(2);
+                        String address = resultSet.getString(3);
+                        double salary = resultSet.getDouble(4);
+
+                        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                        objectBuilder.add("id", id);
+                        objectBuilder.add("name", name);
+                        objectBuilder.add("address", address);
+                        objectBuilder.add("salary", salary);
+                        arrayBuilder.add(objectBuilder.build());
+                    }
+                    JsonObjectBuilder response = Json.createObjectBuilder();
+                    response.add("status", 200);
+                    response.add("message", "Done");
+                    response.add("data", arrayBuilder.build());
+                    writer.print(response.build());
+                    break;
             }
             connection.close();
 
@@ -45,5 +76,11 @@ public class CustomerServlet extends HttpServlet {
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        
     }
 }
