47ee884c6632bea62c04e135b8f1ba0ef99140e8
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-08 17:47:00 +0530

    InbuildCp DataSource Connection

--- InbuildCP/src/servlet/CustomerServlet.java
+++ InbuildCP/src/servlet/CustomerServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author : Yasiru Dahanayaka
@@ -19,9 +25,25 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
+    @Resource(name = "java:comp/env/jdbc/pool")
+    DataSource dataSource;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from Customer");
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-        
+            while (resultSet.next()) {
+                String id = resultSet.getString(1);
+                System.out.println(id);
+            }
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
